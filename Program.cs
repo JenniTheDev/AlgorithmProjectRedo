69ba8fe2fbce1431da13a9b3f78039141a071647
Jenni <justlittleoldmejenn@gmail.com>
2021-07-08 22:12:00 -0700

    Starting Project, Pushing original Project 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace AlgorithmProjectRedo {
+    internal class Program {
+
+        private static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
